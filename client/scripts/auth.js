1366f4bd3ac7ba8485b880ded424e9571a1fcefb
luizduartes <luiz.dsilva@sptech.school>
2026-04-26 13:24:52 -0300

    feature: login/register pages

---
+++ client/scripts/auth.js
@@ -0,0 +1,19 @@
+function auth_login() {
+    let email = ipt_email.value
+    let password = ipt_senha.value
+
+    // Logar com a API web-data-viz
+    console.log(email)
+    console.log(password)
+}
+
+function auth_register() {
+    let username = ipt_username.value
+    let email = ipt_email.value
+    let password = ipt_senha.value
+
+    // Cadastrar com a API web-data-viz
+    console.log(username)
+    console.log(email)
+    console.log(password)
+}
