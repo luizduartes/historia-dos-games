87223df99cd6d464d58944213bc3a3dada6ee6c9
luizduartes <luiz.dsilva@sptech.school>
2026-05-05 21:51:44 -0300

    feature: API public clean

--- server/app.js
+++ server/app.js
@@ -9,24 +9,20 @@ require("dotenv").config({ path: caminho_env });
 
 var express = require("express");
 var cors = require("cors");
-var path = require("path");
 var PORTA_APP = process.env.APP_PORT;
 var HOST_APP = process.env.APP_HOST;
 
 var app = express();
 
-var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var avisosRouter = require("./src/routes/avisos");
 var empresasRouter = require("./src/routes/empresas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
-app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
 app.use("/empresas", empresasRouter);
