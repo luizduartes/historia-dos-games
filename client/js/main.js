b2a571c1b5ee440ac9327454d19745cb9a83f909
luizduartes <luiz.dsilva@sptech.school>
2026-05-10 22:00:25 -0300

    feature: home - bottom bar working

--- client/js/main.js
+++ client/js/main.js
@@ -1,8 +1,11 @@
 import { changeToPage } from "./utils.js"
-import { updateScroll } from "./scrollPage.js"
+import { previousSection, nextSection, updateScroll, updateBottomBar } from "./scrollPage.js"
 
 const welcomeMessage = document.getElementById("welcomeMessage")
 const userButtonMessage = document.getElementById("userButtonMessage")
+
+// Atualizar o conteúdo da Bottom Bar
+updateBottomBar()
 
 // Função para controlar o scroll horizontal da página
 updateScroll()
@@ -16,5 +19,12 @@ function updateUsernameOnPage() {
     welcomeMessage.innerText = sessionStorage.NOME_USUARIO ? `BEM-VINDO, ${String(sessionStorage.NOME_USUARIO).toUpperCase()}` : `BEM-VINDO, EXPLORADOR`
     userButtonMessage.innerText = sessionStorage.NOME_USUARIO ? `USUÁRIO: ${String(sessionStorage.NOME_USUARIO).toUpperCase()}` : `USUÁRIO: CONVIDADO`
 }
+
+// Função de click para os botões de mudar de seção
+const previousSectionButton = document.getElementById("previous-section-button")
+const nextSectionButton = document.getElementById("next-section-button")
+
+previousSectionButton.addEventListener('click', previousSection)
+nextSectionButton.addEventListener('click', nextSection)
 
 updateUsernameOnPage()
--- client/js/scrollPage.js
+++ client/js/scrollPage.js
@@ -1,7 +1,32 @@
 const main = document.querySelector('main')
 const progressFill = document.querySelector('.timeline-fill-progress')
-const sections = document.querySelectorAll('section')
+const sections = Array.from(document.querySelectorAll('section'))
 const timelineProgressMarker = document.querySelectorAll('.timeline-progress-marker')
+
+const ageNames = [
+    "BOOT",
+    "FÓSFORO",
+    "COMPUTADORES",
+    "ARCADES",
+    "PONG",
+    "CONSOLES",
+    "MODERNO",
+    "LEGADO",
+]
+
+const welcomeBottomBarContent = document.getElementById("welcome-bottom-bar-content")
+const navigationBottomBarContent = document.getElementById("navigation-bottom-bar-content")
+
+const previousSectionButton = document.getElementById("previous-section-button")
+const nextSectionButton = document.getElementById("next-section-button")
+const bottomBarAgeIndicatorText = document.getElementById("bottom-bar-age-indicator-text")
+const bottomBarAgeIndicatorProgress = document.getElementById("bottom-bar-age-indicator-progress")
+const bottomBarAgeIndicatorProgressCells = Array.from(bottomBarAgeIndicatorProgress.children)
+const bottomBarPreviousPageIndicator = document.getElementById("bottom-bar-previous-page-indicator")
+const bottomBarNextPageIndicator = document.getElementById("bottom-bar-next-page-indicator")
+console.log(bottomBarAgeIndicatorProgressCells)
+
+let currentSection = 0
 
 let targetScroll = 0
 let currentScroll = 0
@@ -28,13 +53,18 @@ function updateScroll() {
         progressFill.style.width = `${(targetScroll * 100) / maxScroll}%`
     }
 
-    Array.from(sections).forEach((sec, index) => {
+    sections.forEach((sec, index) => {
         if (targetScroll >= sec.offsetLeft - scrollSnapValue && targetScroll <= sec.offsetLeft + scrollSnapValue) {
             targetScroll = sec.offsetLeft
         }
 
         if (targetScroll >= sec.offsetLeft && targetScroll < sec.offsetLeft + sec.offsetWidth) {
             timelineProgressMarker[index].classList.add('timeline-progress-marker-active')
+
+            if (currentSection != index) {
+                currentSection = index
+                updateBottomBar()
+            }
         }
         else {
             timelineProgressMarker[index].classList.remove('timeline-progress-marker-active')
@@ -60,4 +90,62 @@ Array.from(timelineProgressMarker).forEach((element, index) => {
     })
 })
 
-export { updateScroll }
+function previousSection() {
+    if (currentSection > 0) {
+        currentSection -= 1
+        scrollToSection(currentSection)
+        updateBottomBar()
+    }
+}
+
+function nextSection() {
+    if (currentSection < sections.length - 1) {
+        currentSection += 1
+        scrollToSection(currentSection)
+        updateBottomBar()
+    }
+}
+
+function updateBottomBar() {
+    const previousAgeName = ageNames[currentSection - 1]
+    const nextAgeName = ageNames[currentSection + 1]
+
+    if (previousAgeName) {
+        bottomBarPreviousPageIndicator.innerText = ageNames[currentSection - 1]
+        bottomBarPreviousPageIndicator.style.opacity = 1
+        previousSectionButton.style.opacity = 1
+    } else {
+        bottomBarPreviousPageIndicator.style.opacity = 0
+        previousSectionButton.style.opacity = 0
+    }
+
+    if (nextAgeName) {
+        bottomBarNextPageIndicator.innerText = ageNames[currentSection + 1]
+        bottomBarNextPageIndicator.style.opacity = 1
+        nextSectionButton.style.opacity = 1
+    } else {
+        bottomBarNextPageIndicator.style.opacity = 0
+        nextSectionButton.style.opacity = 0
+    }
+
+    bottomBarAgeIndicatorText.innerText = `0${currentSection + 1} / 0${sections.length}`
+
+    bottomBarAgeIndicatorProgressCells.forEach((cell, index) => {
+        if (index <= currentSection) {
+            cell.classList.add("age-indicator-progress-cell-fill")
+        }
+        else {
+            cell.classList.remove("age-indicator-progress-cell-fill")
+        }
+    })
+
+    if (currentSection <= 0) {
+        welcomeBottomBarContent.style.display = "flex"
+        navigationBottomBarContent.style.display = "none"
+    } else {
+        welcomeBottomBarContent.style.display = "none"
+        navigationBottomBarContent.style.display = "flex"
+    }
+}
+
+export { updateScroll, previousSection, nextSection, updateBottomBar }
