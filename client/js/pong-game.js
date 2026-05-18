9a909346ac36d783d0b83f51d54a2464a31aabb7
luizduartes <luiz.dsilva@sptech.school>
2026-05-17 21:30:29 -0300

    feature: pong game

---
+++ client/js/pong-game.js
@@ -0,0 +1,121 @@
+const canvas = document.getElementById("pong-canvas")
+const ctx = canvas.getContext("2d")
+
+// Importando cores do CSS
+const eRoot = document.documentElement
+const styleRoot = getComputedStyle(eRoot)
+const neonColor = styleRoot.getPropertyValue('--color-primary').trim()
+// const darkColor = styleRoot.getPropertyValue('--accent-disabled').trim()
+
+// Propriedades da Raquete do Player
+let racketWidth = 100
+let racketHeight = 20
+let racketX = canvas.width / 2 - racketWidth / 2
+let racketY = canvas.height - racketHeight
+let playerRacketLeft = 0
+let playerRacketRight = 0
+const playerVelocity = 7
+
+// Propriedades da Raquete da Cpu
+let racketCpuX = canvas.width / 2 - racketWidth / 2
+let racketCpuY = 0
+const cpuVelocity = 5
+
+// Propriedades da bola
+let ballRadius = 8
+let ballX = canvas.width / 2
+let ballY = canvas.height / 2
+let ballVelocityX = 4
+let ballVelocityY = 4
+
+function resetBall() {
+    ballX = canvas.width / 2
+    ballY = canvas.height / 2
+    ballVelocityX = (Math.random() > 0.5 ? 1 : -1) * 4
+    ballVelocityY = (Math.random() > 0.5 ? 1 : -1) * 4
+}
+
+function draw() {
+    // Limpando a tela
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    // Cor de preenchimento
+    ctx.fillStyle = neonColor
+    
+    // Raquete do player
+    ctx.fillRect(racketX, racketY, racketWidth, racketHeight)
+    
+    // Raquete da Cpu
+    ctx.fillRect(racketCpuX, racketCpuY, racketWidth, racketHeight)
+    
+    // Bola
+    ctx.beginPath()
+    ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2)
+    ctx.fill()
+    ctx.closePath()
+}
+
+function loop() {
+    // Movimentação do player
+    racketX += (playerRacketLeft + playerRacketRight) * playerVelocity
+    racketX = Math.max(0, Math.min(canvas.width - racketWidth, racketX))
+
+    // Física da bola
+    ballX += ballVelocityX
+    ballY += ballVelocityY
+
+    // Colisão da bola com a parede
+    if (ballX + ballRadius > canvas.width || ballX - ballRadius < 0) {
+        ballVelocityX *= -1
+    }
+
+    // Colisão da bola com o player
+    if (ballY + ballRadius >= racketY && 
+        ballX > racketX && 
+        ballX < racketX + racketWidth) {
+        ballVelocityY *= -1
+        ballY = racketY - ballRadius // Push-out
+    }
+
+    // Colisão da bola com a Cpu
+    if (ballY - ballRadius <= racketHeight && 
+        ballX > racketCpuX && 
+        ballX < racketCpuX + racketWidth) {
+        ballVelocityY *= -1
+        ballY = racketHeight + ballRadius // Push-out
+    }
+
+    // Resetando a bola
+    if (ballY < 0 || ballY > canvas.height) {
+        resetBall()
+    }
+
+    // Movimento da Cpu
+    let cpuRacketCenterX = racketCpuX + (racketWidth / 2)
+    let deadzone = 15 // Evita o tremor da CPU
+
+    if (cpuRacketCenterX < ballX - deadzone) {
+        racketCpuX += cpuVelocity
+    } else if (cpuRacketCenterX > ballX + deadzone) {
+        racketCpuX -= cpuVelocity
+    }
+
+    // Mantendo a Cpu dentro do canvas
+    racketCpuX = Math.max(0, Math.min(canvas.width - racketWidth, racketCpuX))
+
+    draw()
+    requestAnimationFrame(loop)
+}
+
+loop()
+
+// Inputs
+addEventListener('keydown', (e) => {
+    if (e.code == 'KeyD' || e.code == 'ArrowRight') playerRacketRight = 1
+    if (e.code == 'KeyA' || e.code == 'ArrowLeft') playerRacketLeft = -1
+})
+
+addEventListener('keyup', (e) => {
+    if (e.code == 'KeyD' || e.code == 'ArrowRight') playerRacketRight = 0
+    if (e.code == 'KeyA' || e.code == 'ArrowLeft') playerRacketLeft = 0
+})
