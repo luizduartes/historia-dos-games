8008502f7e096ce5d5bcba891858052bdb0dd750
luizduartes <luiz.dsilva@sptech.school>
2026-05-19 19:19:06 -0300

    fix: page buttons path

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,20 +1,6 @@
 import { changeToPage } from "./utils.js";
-const criarContaBtn = document.getElementById("criarContaBtn")
-const voltarLoginBtn = document.getElementById("voltarLoginBtn")
 const loginForm = document.getElementById("loginForm")
 const registerForm = document.getElementById("registerForm")
-
-if (criarContaBtn) {
-    criarContaBtn.addEventListener('click', () => {
-        changeToPage('./register.html')
-    })
-}
-
-if (voltarLoginBtn) {
-    voltarLoginBtn.addEventListener('click', () => {
-        changeToPage('./login.html')
-    })
-}
 
 if (loginForm) {
     loginForm.addEventListener('submit', (e) => {
@@ -195,7 +181,7 @@ async function auth_register() {
 
         if (resposta.ok) {
             showAcceptMessage("CADASTRO REALIZADO", "USUÁRIO CADASTRADO COM SUCESSO!<br>REDIRECIONANDO PARA LOGIN...")
-            setTimeout(() => { changeToPage('./index.html') }, 2000);
+            setTimeout(() => { changeToPage('./login.html') }, 2000);
         } else {
             throw new Error();
         }
