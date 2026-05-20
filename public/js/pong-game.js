348712357a86321786140979e643cc6ac051a141
luizduartes <luiz.dsilva@sptech.school>
2026-05-19 22:52:11 -0300

    feature: score in pong game

--- public/js/pong-game.js
+++ public/js/pong-game.js
@@ -7,6 +7,12 @@ const styleRoot = getComputedStyle(eRoot)
 const neonColor = styleRoot.getPropertyValue('--color-primary').trim()
 // const darkColor = styleRoot.getPropertyValue('--accent-disabled').trim()
 
+// Placar
+let playerScore = 0
+let cpuScore = 0
+let matchSeconds = 0
+let paused = false
+
 // Propriedades da Raquete do Player
 let racketWidth = 100
 let racketHeight = 20
@@ -31,8 +37,13 @@ let ballVelocityY = 4
 function resetBall() {
     ballX = canvas.width / 2
     ballY = canvas.height / 2
-    ballVelocityX = (Math.random() > 0.5 ? 1 : -1) * 4
-    ballVelocityY = (Math.random() > 0.5 ? 1 : -1) * 4
+    ballVelocityX = 0
+    ballVelocityY = 0
+
+    setTimeout(() => {
+        ballVelocityX = (Math.random() > 0.5 ? 1 : -1) * 4
+        ballVelocityY = (Math.random() > 0.5 ? 1 : -1) * 4
+    }, 2000)
 }
 
 function draw() {
@@ -56,58 +67,82 @@ function draw() {
 }
 
 function loop() {
-    // Movimentação do player
-    racketX += (playerRacketLeft + playerRacketRight) * playerVelocity
-    racketX = Math.max(0, Math.min(canvas.width - racketWidth, racketX))
+    if (!paused) {
+        // Movimentação do player
+        racketX += (playerRacketLeft + playerRacketRight) * playerVelocity
+        racketX = Math.max(0, Math.min(canvas.width - racketWidth, racketX))
 
-    // Física da bola
-    ballX += ballVelocityX
-    ballY += ballVelocityY
+        // Física da bola
+        ballX += ballVelocityX
+        ballY += ballVelocityY
 
-    // Colisão da bola com a parede
-    if (ballX + ballRadius > canvas.width || ballX - ballRadius < 0) {
-        ballVelocityX *= -1
+        // Colisão da bola com a parede
+        if (ballX + ballRadius > canvas.width || ballX - ballRadius < 0) {
+            ballVelocityX *= -1
+        }
+
+        // Colisão da bola com o player
+        if (ballY + ballRadius >= racketY && 
+            ballX > racketX && 
+            ballX < racketX + racketWidth) {
+            ballVelocityY *= -1
+            ballY = racketY - ballRadius // Push-out
+        }
+
+        // Colisão da bola com a Cpu
+        if (ballY - ballRadius <= racketHeight && 
+            ballX > racketCpuX && 
+            ballX < racketCpuX + racketWidth) {
+            ballVelocityY *= -1
+            ballY = racketHeight + ballRadius // Push-out
+        }
+
+        // Resetando a bola
+        if (ballY < 0 || ballY > canvas.height) {
+            let scoredBy = ballY < 0 ? "player" : "cpu"
+            resetBall()
+            if (scoredBy == "player") playerGoal()
+            if (scoredBy == "cpu") cpuGoal()
+        }
+
+        // Movimento da Cpu
+        let cpuRacketCenterX = racketCpuX + (racketWidth / 2)
+        let deadzone = 15 // Evita o tremor da CPU
+
+        if (cpuRacketCenterX < ballX - deadzone) {
+            racketCpuX += cpuVelocity
+        } else if (cpuRacketCenterX > ballX + deadzone) {
+            racketCpuX -= cpuVelocity
+        }
+
+        // Mantendo a Cpu dentro do canvas
+        racketCpuX = Math.max(0, Math.min(canvas.width - racketWidth, racketCpuX))
     }
-
-    // Colisão da bola com o player
-    if (ballY + ballRadius >= racketY && 
-        ballX > racketX && 
-        ballX < racketX + racketWidth) {
-        ballVelocityY *= -1
-        ballY = racketY - ballRadius // Push-out
-    }
-
-    // Colisão da bola com a Cpu
-    if (ballY - ballRadius <= racketHeight && 
-        ballX > racketCpuX && 
-        ballX < racketCpuX + racketWidth) {
-        ballVelocityY *= -1
-        ballY = racketHeight + ballRadius // Push-out
-    }
-
-    // Resetando a bola
-    if (ballY < 0 || ballY > canvas.height) {
-        resetBall()
-    }
-
-    // Movimento da Cpu
-    let cpuRacketCenterX = racketCpuX + (racketWidth / 2)
-    let deadzone = 15 // Evita o tremor da CPU
-
-    if (cpuRacketCenterX < ballX - deadzone) {
-        racketCpuX += cpuVelocity
-    } else if (cpuRacketCenterX > ballX + deadzone) {
-        racketCpuX -= cpuVelocity
-    }
-
-    // Mantendo a Cpu dentro do canvas
-    racketCpuX = Math.max(0, Math.min(canvas.width - racketWidth, racketCpuX))
 
     draw()
     requestAnimationFrame(loop)
 }
 
-loop()
+function playerGoal() {
+    playerScore++
+    playerScoreText.innerText = String(playerScore).padStart(2, "0")
+}
+
+function cpuGoal() {
+    cpuScore++
+    cpuScoreText.innerText = String(cpuScore).padStart(2, "0")
+}
+
+// Função para dar play no jogo
+playButton.addEventListener('click', () => {
+    playButton.style.display = "none"
+    play()
+})
+
+function play() {
+    // Começa o loop()
+    loop()
+}
 
 // Inputs
 addEventListener('keydown', (e) => {
@@ -118,4 +153,8 @@ addEventListener('keydown', (e) => {
 addEventListener('keyup', (e) => {
     if (e.code == 'KeyD' || e.code == 'ArrowRight') playerRacketRight = 0
     if (e.code == 'KeyA' || e.code == 'ArrowLeft') playerRacketLeft = 0
+})
+
+addEventListener('keydown', (e) => {
+    if (e.code == 'Escape') paused = !paused
 })
