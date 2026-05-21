ba0ff6f428fface0903a60fc4e21e60d7229777b
luizduartes <luiz.dsilva@sptech.school>
2026-05-21 14:01:03 -0300

    feature: pong gameplay

--- public/js/pong-game.js
+++ public/js/pong-game.js
@@ -5,13 +5,13 @@ const ctx = canvas.getContext("2d")
 const eRoot = document.documentElement
 const styleRoot = getComputedStyle(eRoot)
 const neonColor = styleRoot.getPropertyValue('--color-primary').trim()
-// const darkColor = styleRoot.getPropertyValue('--accent-disabled').trim()
 
 // Placar
 let playerScore = 0
 let cpuScore = 0
 let matchSeconds = 0
 let paused = false
+
 
 // Propriedades da Raquete do Player
 let racketWidth = 100
@@ -25,14 +25,29 @@ const playerVelocity = 7
 // Propriedades da Raquete da Cpu
 let racketCpuX = canvas.width / 2 - racketWidth / 2
 let racketCpuY = 0
-const cpuVelocity = 5
+const cpuDeadzone = 20
+const cpuMinVelocity = 3
+const cpuMaxVelocity = 5
+let cpuVelocity = 1
 
 // Propriedades da bola
-let ballRadius = 8
+const ballRadius = 8
 let ballX = canvas.width / 2
 let ballY = canvas.height / 2
-let ballVelocityX = 4
-let ballVelocityY = 4
+const ballMaxVelocity = 4
+let ballVelocityX = 0
+let ballVelocityY = ballMaxVelocity
+const accelerationBallColision = 1.05
+
+resetBall()
+
+function normalized(valor, minAtual, maxAtual, minDesejado, maxDesejado) {
+  // 1. Normaliza o valor para a escala de 0 a 1
+  const normalizedValue = (valor - minAtual) / (maxAtual - minAtual);
+  
+  // 2. Aplica o valor na nova escala desejada
+  return normalizedValue * (maxDesejado - minDesejado) + minDesejado;
+}
 
 function resetBall() {
     ballX = canvas.width / 2
@@ -41,8 +56,8 @@ function resetBall() {
     ballVelocityY = 0
 
     setTimeout(() => {
-        ballVelocityX = (Math.random() > 0.5 ? 1 : -1) * 4
-        ballVelocityY = (Math.random() > 0.5 ? 1 : -1) * 4
+        ballVelocityX = (Math.random() > 0.5 ? 1 : -1) * ballMaxVelocity
+        ballVelocityY = (Math.random() > 0.5 ? 1 : -1) * ballMaxVelocity
     }, 2000)
 }
 
@@ -82,19 +97,29 @@ function loop() {
         }
 
         // Colisão da bola com o player
-        if (ballY + ballRadius >= racketY && 
+        if
+        (
+            ballY + ballRadius >= racketY && 
             ballX > racketX && 
-            ballX < racketX + racketWidth) {
-            ballVelocityY *= -1
-            ballY = racketY - ballRadius // Push-out
+            ballX < racketX + racketWidth
+        )
+        {
+            ballVelocityY *= -(accelerationBallColision)
+            ballVelocityX += normalized(ballX, racketX, racketX + racketWidth, -1, 1) * ballMaxVelocity
+            ballY = racketY - ballRadius
         }
 
         // Colisão da bola com a Cpu
-        if (ballY - ballRadius <= racketHeight && 
+        if
+        (
+            ballY - ballRadius <= racketHeight && 
             ballX > racketCpuX && 
-            ballX < racketCpuX + racketWidth) {
-            ballVelocityY *= -1
-            ballY = racketHeight + ballRadius // Push-out
+            ballX < racketCpuX + racketWidth
+        )
+        {
+            ballVelocityY *= -(accelerationBallColision)
+            ballVelocityX += normalized(ballX, racketCpuX, racketCpuX + racketWidth, -1, 1) * ballMaxVelocity
+            ballY = racketHeight + ballRadius
         }
 
         // Resetando a bola
@@ -105,13 +130,17 @@ function loop() {
             if (scoredBy == "cpu") cpuGoal()
         }
 
+        // Variando a velocidade da Cpu
+        if (Math.random() > 0.7) {
+            cpuVelocity = normalized(Math.random(), 0, 1, cpuMinVelocity, cpuMaxVelocity)
+        }
+
         // Movimento da Cpu
         let cpuRacketCenterX = racketCpuX + (racketWidth / 2)
-        let deadzone = 15 // Evita o tremor da CPU
 
-        if (cpuRacketCenterX < ballX - deadzone) {
+        if (cpuRacketCenterX < ballX - cpuDeadzone) {
             racketCpuX += cpuVelocity
-        } else if (cpuRacketCenterX > ballX + deadzone) {
+        } else if (cpuRacketCenterX > ballX + cpuDeadzone) {
             racketCpuX -= cpuVelocity
         }
 
