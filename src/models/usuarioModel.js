672d8c87cf61923687e090af3610d9c9518ea1ab
luizduartes <luiz.dsilva@sptech.school>
2026-05-24 03:51:08 -0300

    feature: update user stats

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -19,12 +19,39 @@ function cadastrar(username, email, senha) {
     return database.executar(instrucaoSql)
 }
 
-function addMatch(id, playerScore, cpuScore, matchSeconds, matchResult) {
-    let instrucaoSql = `
-        INSERT INTO partida (id_usuario, pontuacao_player, pontuacao_cpu, duracao_segundos, resultado) VALUES ('${id}', '${playerScore}', '${cpuScore}', '${matchSeconds}', '${matchResult}');
+async function addMatch(id, playerScore, cpuScore, matchSeconds, matchResult) {
+    console.log("Iniciando registro de partida...")
+    let sqlInsertPartida = `
+        INSERT INTO partida (id_usuario, pontuacao_player, pontuacao_cpu, duracao_segundos, resultado) VALUES
+        ('${id}', '${playerScore}', '${cpuScore}', '${matchSeconds}', '${matchResult}');
     `
-    console.log("Executando a instrução SQL: \n" + instrucaoSql)
-    return database.executar(instrucaoSql)
+
+    console.log("Executando INSERT da partida...")
+    await database.executar(sqlInsertPartida)
+
+    let sqlUpdateUsuario = ""
+
+    if (matchResult == "VITORIA") {
+        sqlUpdateUsuario = `    
+            UPDATE usuario SET
+                vitorias = vitorias + 1,
+                win_streak_atual = win_streak_atual + 1,
+                melhor_win_streak = IF(win_streak_atual > melhor_win_streak, win_streak_atual, melhor_win_streak)
+            WHERE id = ${id};
+        `
+    } else if (matchResult == "DERROTA") {
+        sqlUpdateUsuario = `
+            UPDATE usuario SET
+                derrotas = derrotas + 1,
+                win_streak_atual = 0
+            WHERE id = ${id};
+        `
+    }
+
+    if (sqlUpdateUsuario) {
+        console.log("Executando UPDATE do usuário...")
+        return database.executar(sqlUpdateUsuario)
+    }
 }
 
 module.exports = {
