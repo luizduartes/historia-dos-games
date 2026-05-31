5ea4a03a74c9cc25b00da19539ca8227d9acca3c
luizduartes <luiz.dsilva@sptech.school>
2026-05-30 23:37:53 -0300

    style: user achievements page

--- public/js/user-achievements.js
+++ public/js/user-achievements.js
@@ -1,3 +1,72 @@
+// -----------> CARROSSEL DAS CONQUISTAS DESBLOQUEADAS
+const achievementsWrapperViewport = document.getElementById("achievementsWrapperViewport")
+const achievementsWrapper = achievementsWrapperViewport.querySelector(".achievements-wrapper")
+const achievementsCards = Array.from(achievementsWrapper.children)
+
+const achievementNavigationDisplacement = achievementsWrapperViewport.scrollWidth / achievementsCards.length
+
+achievementNavigationPreviousButton.addEventListener('click', () => {
+    achievementsWrapperViewport.scrollLeft -= achievementNavigationDisplacement
+
+    updateAchievementNavigationButtons(achievementsWrapperViewport.scrollLeft - achievementNavigationDisplacement)
+})
+
+achievementNavigationNextButton.addEventListener('click', () => {
+    achievementsWrapperViewport.scrollLeft += achievementNavigationDisplacement
+
+    updateAchievementNavigationButtons(achievementsWrapperViewport.scrollLeft + achievementNavigationDisplacement)
+})
+
+
+function updateAchievementNavigationButtons(newScrollPos) {
+    if (newScrollPos <= 0) {
+        achievementNavigationPreviousButton.classList.add("achievement-navigation-button--disabled")
+    } else {
+        achievementNavigationPreviousButton.classList.remove("achievement-navigation-button--disabled")
+    }
+
+    if (newScrollPos + achievementsWrapperViewport.clientWidth >= achievementsWrapperViewport.scrollWidth) {
+        achievementNavigationNextButton.classList.add("achievement-navigation-button--disabled")
+    } else {
+        achievementNavigationNextButton.classList.remove("achievement-navigation-button--disabled")
+    }
+}
+
+// -----------> CARROSSEL DAS CONQUISTAS BLOQUEADAS
+const lockedAchievementsWrapperViewport = document.getElementById("lockedAchievementsWrapperViewport")
+const lockedAchievementsWrapper = lockedAchievementsWrapperViewport.querySelector(".achievements-wrapper")
+const lockedAchievementsCards = Array.from(lockedAchievementsWrapper.children)
+
+const lockedAchievementNavigationDisplacement = lockedAchievementsWrapperViewport.scrollWidth / lockedAchievementsCards.length
+
+lockedAchievementNavigationPreviousButton.addEventListener('click', () => {
+    lockedAchievementsWrapperViewport.scrollLeft -= lockedAchievementNavigationDisplacement
+
+    updateLockedAchievementNavigationButtons(lockedAchievementsWrapperViewport.scrollLeft - lockedAchievementNavigationDisplacement)
+})
+
+lockedAchievementNavigationNextButton.addEventListener('click', () => {
+    lockedAchievementsWrapperViewport.scrollLeft += lockedAchievementNavigationDisplacement
+
+    updateLockedAchievementNavigationButtons(lockedAchievementsWrapperViewport.scrollLeft + lockedAchievementNavigationDisplacement)
+})
+
+
+function updateLockedAchievementNavigationButtons(newScrollPos) {
+    if (newScrollPos <= 0) {
+        lockedAchievementNavigationPreviousButton.classList.add("achievement-navigation-button--disabled")
+    } else {
+        lockedAchievementNavigationPreviousButton.classList.remove("achievement-navigation-button--disabled")
+    }
+    
+    if (newScrollPos + lockedAchievementsWrapperViewport.clientWidth >= lockedAchievementsWrapperViewport.scrollWidth) {
+        lockedAchievementNavigationNextButton.classList.add("achievement-navigation-button--disabled")
+    } else {
+        lockedAchievementNavigationNextButton.classList.remove("achievement-navigation-button--disabled")
+    }
+}
+
+// -----------> CHART DA PORCENTAGEM DE CONQUISTAS DESBLOQUEADAS
 const ctx_chartAchievements = document.getElementById("chartAchievementsPercent")
 let achievementsChart = null
 
